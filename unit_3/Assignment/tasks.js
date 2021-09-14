42f00d51c4b98da14d2979bc1d65b13d2247141e
iscar desktop <scaristhelord@gmail.com>
2021-09-14 14:34:46 +0530

    Final part of the constructors assignment completed

--- unit_3/Assignment/tasks.js
+++ unit_3/Assignment/tasks.js
@@ -1,3 +1,6 @@
+var tCounter = 0;
+
+
 function tasks(name) {
     this.name = name;
     this.date = new Date();
@@ -27,11 +30,13 @@ thead.appendChild(th3);
 tab.appendChild(thead);
 
 function myFunc() {
+    tCounter++;
+    counter.innerHTML = tCounter;
     var t1 = new tasks(field.value);
     field.value = "";
-
+    
     var row = document.createElement("tr");
-
+    
     var c1 = document.createElement("td");
     c1.innerHTML = t1.name;
     var c2 = document.createElement("td");
@@ -39,20 +44,32 @@ function myFunc() {
     var c3 = document.createElement("td");
     
     var btn = document.createElement("button");
-    btn.setAttribute("class", "delBtn");
+    btn.setAttribute("class", "cngBtn");
+    btn.setAttribute("style", "background: none;border: none;width: 100%");
     c3.appendChild(btn);
-
+    
     btn.innerHTML = t1.status;
+
+    var btn1 = document.createElement("button");
+    var btn2 = document.createElement("button");
+    btn1.innerHTML = "Done";
+    btn2.innerHTML = "Remove";
+    btn1.setAttribute("class", "btn1");
+    btn2.setAttribute("class", "btn2");
 
     row.appendChild(c1);
     row.appendChild(c2);
     row.appendChild(c3);
+
+    row.appendChild(btn1);
+    row.appendChild(btn2);
+    
     tab.appendChild(row);
     document.body.appendChild(tab);
 }
 
 function toggle_task(e){
-    if(!e.target.classList.contains('delBtn')){
+    if(!e.target.classList.contains('cngBtn')){
         return;
     }
 
@@ -64,4 +81,34 @@ function toggle_task(e){
         btn.innerHTML = "false";
 }
 
+function done(e){
+    if(!e.target.classList.contains('btn1')){
+        return;
+    }
+    var btn = e.target.parentElement;
+    btn.setAttribute("style", "background-color: green");
+    var ch = btn.getElementsByTagName("td");
+    ch[0].setAttribute("style", "text-decoration: line-through;");
+    ch[1].setAttribute("style", "text-decoration: line-through;");
+    ch[2].querySelector("button").style.textDecoration = "line-through";
+}
+
+function remove(e){
+    if(!e.target.classList.contains('btn2')){
+        return;
+    }
+    var btn = e.target.parentElement;
+    btn.remove();
+    tCounter--;
+    counter.innerHTML = tCounter;
+}
+
 tab.addEventListener('click', toggle_task);
+tab.addEventListener('click', done);
+tab.addEventListener('click', remove);
+
+var counter = document.createElement("h1");
+counter.innerHTML = tCounter;
+counter.setAttribute("style", "position: absolute");
+counter.style.right = "5%";
+document.body.appendChild(counter);
