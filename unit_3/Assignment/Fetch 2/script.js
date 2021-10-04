8f41ea0fbf40500ace69bdcf925d5c46590f8095
iscar desktop <scaristhelord@gmail.com>
2021-10-04 15:30:52 +0530

    Almost completed the pagination part

--- unit_3/Assignment/Fetch 2/script.js
+++ unit_3/Assignment/Fetch 2/script.js
@@ -4,28 +4,74 @@ var category = "";
 var apiKey = "&apiKey=f09330388d054ea8b405c7cb47a9516d"
 var country = "&country=in";
 var srch = doc.querySelector(".navbar").querySelector("input");
+var currentPage = 1;
 
 function chngFunc(){
     // console.log(srch.value);
     request();
 }
 
+function createPagination(){
+    const pagination = document.createElement("div");
+    pagination.setAttribute("class", "pagination");
+    const prev = document.createElement("button");
+    const curr = document.createElement("button");
+    const next = document.createElement("button");
+
+    prev.textContent = "previous";
+    next.textContent = "next";
+    curr.textContent = currentPage;
+
+    prev.name = currentPage-1;
+    curr.name = currentPage;
+    next.name = currentPage+1;
+
+    if(currentPage == 1){
+        prev.disabled = true;
+    }
+
+    pagination.append(prev,curr,next);
+    doc.append(pagination);
+}
+
 function display(data){
-    var heading = document.createElement("h2");
-    var author = document.createElement("h4");
-    var content = document.createElement("p");
-    
-    author.style.color = "red";
-    content.style.color = "blue";
-
-    heading.textContent = data.title;
-    author.textContent = `Source: ${data.source.name}`;
-    content.textContent = data.content;
-
     // console.log(data);
-    doc.append(heading);
-    doc.append(author);
-    doc.append(content);
+    if(doc.querySelector(".cont")){
+        doc.querySelector(".cont").remove();
+    }
+    if(doc.querySelector(".pagination")){
+        doc.querySelector(".pagination").remove();
+    }
+    var cont = document.createElement("div");
+    cont.setAttribute("class", "cont");
+    for(var i=0;i<data.articles.length;i++){
+        
+        var heading = document.createElement("h2");
+        var author = document.createElement("h4");
+        var content = document.createElement("p");
+        
+        author.style.color = "red";
+        content.style.color = "blue";
+    
+        heading.textContent = data.articles[i].title;
+        author.textContent = `Source: ${data.articles[i].source.name}`;
+        content.textContent = data.articles[i].content;
+    
+        // console.log(data);
+        cont.append(heading);
+        cont.append(author);
+        cont.append(content);
+    }
+    doc.append(cont);
+    
+    createPagination();
+    doc.querySelector(".pagination").addEventListener("click", handleChange);
+    
+}
+
+function handleChange(){
+    
+    console.log(event.target);
 }
 
 async function request() {
@@ -39,28 +85,31 @@ async function request() {
         category = "&category=technology"
     }
     
-    // var url = `https://newsapi.org/v2/top-headlines?pageSize=5${country}${category}${apiKey}`;
+    // var url = `https://newsapi.org/v2/top-headlines?pageSize=5&page=${currentPage}${country}${category}${apiKey}`;
 
 
     if(cList.contains("home") && srch.value){
         console.log(srch.value);
         var topic = srch.value;
-        // url = `https://newsapi.org/v2/everything?q=${topic}&pageSize=5&from=2021-09-04&sortBy=publishedAt&apiKey=f09330388d054ea8b405c7cb47a9516d`
+        // url = `https://newsapi.org/v2/everything?q=${topic}&pageSize=5&page=${currentPage}&from=2021-09-04&sortBy=publishedAt&apiKey=f09330388d054ea8b405c7cb47a9516d`
     }
     
     try {
-        console.log(url);
+        // console.log(url);
         let res = await fetch(
             `${url}`
         )
         let data = await res.json()
-        for(var i=0; i<data.articles.length;i++){
-            display(data.articles[i]);
-        }
+        // for(var i=0; i<data.articles.length;i++){
+        //     display(data.articles[i]);
+        // }
+        display(data)
     }
     catch (err){
         console.log(err,"something went wrong");
         alert("something went wrong");
     }
 }
-request();
+window.addEventListener("load", function(){
+    request();
+})
