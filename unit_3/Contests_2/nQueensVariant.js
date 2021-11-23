0a6ff2964527d0b995488a6ffa1c2dbc7a2ded2d
iscar desktop <scaristhelord@gmail.com>
2021-11-23 13:59:23 +0530

    n queens variant question solved

--- unit_3/Contests_2/nQueensVariant.js
+++ unit_3/Contests_2/nQueensVariant.js
@@ -1,33 +1,32 @@
-var N = 0;
+var count = 0;
 
-function placeQueen(board, col) {
-    if (col >= N) {
-        console.log(board);
-        return true;
+function placeQueen(board, row, N) {
+    if (row == N) {
+        // console.log(board);
+        count++;
+        return;
     }
 
     for(let i=0;i<N;i++){
-        if(isSafe(board, i, col)){
-            board[i][col] = 1;
-            if(placeQueen(board,col+1)){
-                return true;
-            }
-            board[i][col] = 0;
+        if(isSafe(board, row, i)){
+            board[row][i] = 1;
+            placeQueen(board,row+1,N);
+            board[row][i] = 0;
         }
     }
-    return false;
+    
 }
 
 function isSafe(board, row, col) {
-    for (let i = 0; i < col; i++){
-        if (board[row][i])
+    for (let i = 0; i < row; i++){
+        if (board[i][col])
             return false;
     }
-    for (let i = row, j = col; i >= 0 && j >= 0; i--, j--){
+    for (let i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--){
         if (board[i][j])
             return false;
     }
-    for (let i = row, j = col; j >= 0 && i < N; i++, j--){
+    for (let i = row-1, j = col+1; i >= 0 && j < board.length; i--, j++){
         if (board[i][j])
             return false;
     }
@@ -43,10 +42,11 @@ function runProgram(input) {
             board[i][j] = 0;
         }
     }
-    N = n;
 
-    placeQueen(board, 0);
+    placeQueen(board, 0, n);
+    console.log(count);
 }
+
 
 
 runProgram(`4`);
