91967c681ba1dfd1ec3d6dcae8e438fc2bbc3b44
iscar desktop <scaristhelord@gmail.com>
2021-12-14 11:47:08 +0530

    Unit 3 Async contest 10 remaining questions solved

--- unit_3_async/Contests_10/minimumPlatformsRequired.js
+++ unit_3_async/Contests_10/minimumPlatformsRequired.js
@@ -1,34 +1,40 @@
-function runProgram(input){
+function runProgram(input) {
     input = input.split("\n");
     let n = Number(input[0]);
     let arr = input[1].split(" ");
     let dep = input[2].split(" ");
-    let arr2 = [];
-    let dep2 = [];
-    for(let i=0;i<n;i++){
-        let str = arr[i];
-        let res = "";
-        for(let j=0;j<str.length;j++){
-            if(str[j] >= '0' && str[j] <= '9'){
-                res += str[j];
-            }
-        }
-        arr2.push(Number(res));
 
-        str = dep[i];
-        res = "";
-        for(let j=0;j<str.length;j++){
-            if(str[j] >= '0' && str[j] <= '9'){
-                res += str[j];
-            }
-        }
-        dep2.push(Number(res));
+    for (let i = 0; i < n; i++) {
+        arr[i] = Number(arr[i].split(":").join(""));
+        dep[i] = Number(dep[i].split(":").join(""));
     }
 
-    console.log(arr2,dep2);
-    // console.log(n,arr,dep)
+    arr.sort((a,b) => a - b);
+    dep.sort((a,b) => a - b);
+    // console.log(arr2,dep2);
+    let i=0;
+    let j=0;
+    let count = 0;
+    let ans = -1;
+    while(i<n && j<n){
+        if(arr[i] < dep[j]){
+            count++
+            i++;
+        }
+        if(dep[j] <= arr[i]){
+            if(count > ans){
+                ans = count;
+            }
+            count--;
+            j++;
+        }
+    }
+    if(count > ans){
+        ans = count;
+    }
+    console.log(ans)
 }
 
 runProgram(`6
-9:00 9:40 9:50 11:00 15:00 18:00
-9:10 12:00 11:20 11:30 19:00 20:00`);
+9:05 10:40 10:50 10:10 11:00 11:15
+9:10 11:00 11:20 11:30 13:00 20:00`);
--- unit_3_async/Contests_10/stealAsMuchAsYouCan.js
+++ unit_3_async/Contests_10/stealAsMuchAsYouCan.js
@@ -1,17 +1,36 @@
 function runProgram(input){
-    input = input.split("\n");
+    input = input.trim().split("\n");
     let C = Number(input[0]);
     let n = Number(input[1]);
-    let val = input[2].split(" ").map(Number);
-    let w = input[3].split(" ").map(Number);
-    let profit = 0;
-
+    let val = input[2].trim().split(" ").map(Number);
+    let wei = input[3].trim().split(" ").map(Number);
+    let wvr = [];
     for(let i=0;i<n;i++){
-        
+        let tom = {};
+        tom['v'] = val[i];
+        tom['w'] = wei[i];
+        wvr.push(tom);
     }
+    wvr.sort((a,b) => {
+        let r1 = a.v/a.w;
+        let r2 = b.v/b.w;
+        return r2 - r1
+    })
+    let profit = 0;
+    
+    for(let i=0;i<n;i++){
+        if((C-wvr[i].w) >= 0){
+            profit += wvr[i].v;
+            C -= wvr[i].w;
+        }else{
+            profit += wvr[i].v*(C/wvr[i].w)
+            C -= wvr[i].w * (C/wvr[i].w);
+        }
+    }
+    console.log(Math.round(profit))
 }
 
-runProgram(`50 
-3
-60 100 120 
-10 20 30`);
+runProgram(`520
+10
+206 377 199 189 244 58 118 297 132 181
+100 158 128 134 140 142 161 148 149 125`);
