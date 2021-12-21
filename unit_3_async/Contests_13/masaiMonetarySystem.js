a7e6682f0247a02984597f904944dfa43b7bfbda
iscar desktop <scaristhelord@gmail.com>
2021-12-21 20:30:20 +0530

    Finally solved it after watching editorial video

--- unit_3_async/Contests_13/masaiMonetarySystem.js
+++ unit_3_async/Contests_13/masaiMonetarySystem.js
@@ -1,27 +1,21 @@
 let dp = [];
 
 function money(n) {
-    // if (dp[n] || dp[n] == 0) {
-    //     return dp[n];
-    // }
+    if (dp[n] || dp[n] == 0) {
+        return dp[n];
+    }
     if (n <= 0) {
-        dp[0] = 0;
         return 0;
     }
     if (n == 1) {
-        dp[n] = 1;
+        dp[n] = n;
         return 1;
     }
-    if (dp[n] != -1) {
-        return dp[n];
-    }
-    // if (n == 1) {
-    //     dp[1] = 1;
-    //     return 1;
-    // }
-
-    let val = money(parseInt(n / 2)) + money(parseInt(n / 3)) + money(parseInt(n / 4));
-    dp[n] = (val > dp[n] ? val : dp[n]);
+    let v1 = money(parseInt(n/2));
+    let v2 = money(parseInt(n/3));
+    let v3 = money(parseInt(n/4));
+    let val = v1+v2+v3;
+    dp[n] = (val>n?val:n);
     return dp[n]
 }
 
@@ -29,20 +23,19 @@ function runProgram(input) {
     let arr = input.split("\n").map(Number);
     let n = arr.length;
     for (let i = 0; i < n; i++) {
-        dp = new Array(n);
-        dp.fill(-1);
+        dp = []
         console.log(money(arr[i]))
-        // console.log(dp)
     }
 }
 
 runProgram(`12
-1
-2
-3
-4
-100
-1000000000
-12342135
-1234213
-5673457`);
+317
+381
+910
+4176025
+3349949
+684407731
+861466906
+815968858
+682390379
+548900521`);
