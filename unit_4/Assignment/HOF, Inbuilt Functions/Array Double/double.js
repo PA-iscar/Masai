684efecc8684523d9141c2914653f1e289ea86c4
Ãßhìshêk ãrørâ <abhiaro27@gmail.com>
2022-01-17 17:33:12 +0530

    Made changes to the HOF assignments

--- unit_4/Assignment/HOF, Inbuilt Functions/Array Double/double.js
+++ unit_4/Assignment/HOF, Inbuilt Functions/Array Double/double.js
@@ -1,4 +1,5 @@
 let arr = [2, 3, 5, 7, 9];
-let arr2 = arr.map(a => a*2);
+let isDouble = a => a*2;
+let arr2 = arr.map(isDouble);
 
 console.log(arr2)
--- unit_4/Assignment/HOF, Inbuilt Functions/Array Square/square.js
+++ unit_4/Assignment/HOF, Inbuilt Functions/Array Square/square.js
@@ -1,4 +1,5 @@
 let arr = [2, 3, 5, 7, 9];
-let arr2 = arr.map(a => Math.pow(a,2));
+let isSquare = a => Math.pow(a,2);
+let arr2 = arr.map(isSquare);
 
 console.log(arr2)
