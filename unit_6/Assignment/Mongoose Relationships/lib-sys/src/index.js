a8b20271edc1c467352d35f3241e086640f2fcfe
Abhishek <abhiaro27@gmail.com>
2022-03-23 13:51:02 +0530

    models for lib-sys created

---
+++ unit_6/Assignment/Mongoose Relationships/lib-sys/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const PORT = 8000;
+
+const app = express()
+
+
+
+app.listen(PORT, ()=>{
+    console.log("listening to port: ", PORT)
+})
---
+++ unit_6/Assignment/Mongoose Relationships/lib-sys/src/models/section.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const sectionSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  books: [{ type: mongoose.Schema.Types.ObjectId, ref: "book" }],
+});
+
+const Section = mongoose.model("section", sectionSchema);
+
+module.exports = Section;
